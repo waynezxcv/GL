fc799e890ac0a35a289f48dadf80c5964380f8dd
waynezxcv <liuweiself@126.com>
2017-04-26 10:13:45 +0800

    Initial Commit

---
+++ LearnOpenGL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearnOpenGL
+//
+//  Created by 刘微 on 2017/4/26.
+//  Copyright © 2017年 waynezxcv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LearnOpenGL/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LearnOpenGL
+//
+//  Created by 刘微 on 2017/4/26.
+//  Copyright © 2017年 waynezxcv. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
